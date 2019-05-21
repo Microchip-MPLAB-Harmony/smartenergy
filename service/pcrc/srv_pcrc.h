3295cd3c1ec5be9ef11e05e069823b86594f1466
Cristian Guemes <cristian.guemes@microchip.com>
2019-05-21 16:47:03 +0200

    [SMS-848] Review internal documentation for CRC service

--- service/pcrc/srv_pcrc.h
+++ service/pcrc/srv_pcrc.h
@@ -45,14 +45,11 @@
 #ifndef SRV_PCRC_H    // Guards against multiple inclusion
 #define SRV_PCRC_H
 
-
 // *****************************************************************************
 // *****************************************************************************
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-/*  This section lists the other files that are included in this file.
-*/
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -73,6 +70,17 @@
 // *****************************************************************************
 // *****************************************************************************
 
+// *****************************************************************************
+/* USI Header Types
+
+   Summary
+    The list of all header types supported by PRIME spec and specific type to 
+    use with USI service.
+
+   Remarks:
+    None
+*/
+
 typedef enum 
 {
     PCRC_HT_GENERIC = 0,
@@ -80,6 +88,16 @@ typedef enum
     PCRC_HT_BEACON = 2,
     PCRC_HT_USI = 3
 }PCRC_HEADER_TYPE;
+
+// *****************************************************************************
+/* CRC Types
+
+   Summary
+    The list of all CRC types supported (8, 16 and 32 bits).
+
+   Remarks:
+    None
+*/
 
 typedef enum 
 {
@@ -89,13 +107,94 @@ typedef enum
     PCRC_NOCRC = 3
 }PCRC_CRC_TYPE;
 
+/* SRV_PCRC Handle Macro: Invalid CRC */  
 #define PCRC_INVALID     0xFFFFFFFF
-        
+
+/* SRV_PCRC Handle Macro: SNA size */  
+#define PCRC_SNA_SIZE 6
+
+// *****************************************************************************
+/* Function:
+    uint32_t SRV_PCRC_GetValue(
+      uint8_t *pData, 
+      size_t length,
+      PCRC_HEADER_TYPE hdrType, 
+      PCRC_CRC_TYPE crcType,
+      uint32_t initValue
+    );
+
+  Summary:
+    Function to obtain the CRC value of the data.
+
+  Description:
+    This routine gets the CRC value of the data passed in arguments.
+
+  Precondition:
+    None.
+
+  Parameters:
+    pData  -    Pointer to the content of data to obtain CRC value.
+
+    length -    Length of the data to obtain CRC value.
+
+    hdrType -   Header type to determine what method to obtain CRC 
+                should be used. It is different for USI and PRIME 
+                packets (GENERIC, PROMOTION and BEACON).
+
+    crcType -   CRC type(8, 16 or 32 bits). Valid only in case to use 
+                PCRC_HT_USI.
+
+    initValue - First value to start obtaining CRC value. Valid only in case 
+                to use PCRC_HT_USI.
+
+  Returns:
+    If successful, the routine returns a valid CRC value.
+    If an error occurs, the return value is PCRC_INVALID. Error can occur if
+    hdrType or crcType are wrong.
+
+  Example:
+    <code>
+    uint32_t valueTmp32;
+    
+    valueTmp32 = SRV_PCRC_GetValue(pData, length, PCRC_HT_USI, PCRC_CRC32);
+    </code>
+
+  Remarks:
+    None.
+*/        
 uint32_t SRV_PCRC_GetValue(uint8_t *pData, size_t length,
-        PCRC_HEADER_TYPE hdrType, PCRC_CRC_TYPE crcType);
+        PCRC_HEADER_TYPE hdrType, PCRC_CRC_TYPE crcType, uint32_t initValue);
 
-void SRV_PCRC_SetInitialValue(uint32_t value);
+// *****************************************************************************
+/* Function:
+    void SRV_PCRC_SetSNAValue (uint8_t* sna);
 
+  Summary:
+    Function to set SNA value as initial value to get the CRC value of the data.
+
+  Description:
+    This routine gets the CRC value of the data passed in arguments.
+
+  Precondition:
+    None.
+
+  Parameters:
+    sna    -    Sub-Network Address used in PRIME specification to obtain
+                CRC values in some PRIME control packets.
+
+  Returns:
+    None.
+
+  Example:
+    <code>
+    uint8_t sna[PCRC_SNA_SIZE];
+    
+    SRV_PCRC_SetSNAValue(sna);
+    </code>
+
+  Remarks:
+    None.
+*/  
 void SRV_PCRC_SetSNAValue (uint8_t* sna);
 
 #endif //SRV_PCRC_H
