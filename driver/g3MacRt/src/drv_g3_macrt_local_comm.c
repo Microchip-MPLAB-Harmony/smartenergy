b359e36aa5f45cae32745b7862329f76ebf10fc0
Cristian Guemes <cristian.guemes@microchip.com>
2022-03-23 13:26:26 +0100

    SMS-2643: G3 MAC RT: Change order in events generation

--- driver/g3MacRt/src/drv_g3_macrt_local_comm.c
+++ driver/g3MacRt/src/drv_g3_macrt_local_comm.c
@@ -321,18 +321,6 @@ void DRV_G3_MACRT_Task(void)
         gG3MacRtObj->evTxCfm = false;
     }
     
-    if (gG3MacRtObj->evDataIndLength)
-    {   
-        if (gG3MacRtObj->dataIndCallback)
-        {
-            /* Report to upper layer */
-            gG3MacRtObj->dataIndCallback(gG3RxData, gG3MacRtObj->evDataIndLength);
-        }
-        
-        /* Reset event flag */
-        gG3MacRtObj->evDataIndLength = 0;
-    }
-    
     if (gG3MacRtObj->evRxParams)
     {   
         if (gG3MacRtObj->rxParamsIndCallback)
@@ -343,6 +331,18 @@ void DRV_G3_MACRT_Task(void)
         
         /* Reset event flag */
         gG3MacRtObj->evRxParams = false;
+    }
+    
+    if (gG3MacRtObj->evDataIndLength)
+    {   
+        if (gG3MacRtObj->dataIndCallback)
+        {
+            /* Report to upper layer */
+            gG3MacRtObj->dataIndCallback(gG3RxData, gG3MacRtObj->evDataIndLength);
+        }
+        
+        /* Reset event flag */
+        gG3MacRtObj->evDataIndLength = 0;
     }
     
     if (gG3MacRtObj->evMacSnifLength)
