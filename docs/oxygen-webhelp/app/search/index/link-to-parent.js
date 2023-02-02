7ca5c06dbfe9ec59591bbf3524435cff46a41120
Cristian Guemes <cristian.guemes@microchip.com>
2023-02-02 09:17:53 +0100

    SMS-3130: Update docs folder from Publication Manager web help output (SDL)

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {226:111,13:111,8:111,181:111,71:111,137:111};
+return {100:19,143:117,240:19,226:19,234:117,76:117,4:19,10:117,8:19,198:19,19:-1,20:19,187:117,15:117,41:19};
 });
