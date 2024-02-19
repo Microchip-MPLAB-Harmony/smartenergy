f99feb3abfd1e68b6c4ac06e18cf97f7fd212066
David Sancho <david.sancho@microchip.com>
2024-02-19 11:12:47 +0100

    SMS-3864: Reverted unintended modifications manually done from MPLAB.

--- apps/dual_core_demo/firmware/DualCoreDemo_Core1.X/dist/DualCoreDemo_Core1/production/DualCoreDemo_Core1.h
+++ apps/dual_core_demo/firmware/DualCoreDemo_Core1.X/dist/DualCoreDemo_Core1/production/DualCoreDemo_Core1.h
@@ -1,4 +1,12 @@
 #ifndef __BIN2HEX_XC32_SECONDARY_RECORD_H
 #define __BIN2HEX_XC32_SECONDARY_RECORD_H
+typedef struct __xc32_section_record {
+    void *sectionDestinationAddress;
+    const unsigned char *sectionData;
+} __xc32_section_record;
+
+extern const __xc32_section_record __attribute__((space(prog))) __secondary_image_sections[];
+extern const void __xc32_LoadSecondarySection(__xc32_section_record *section_to_load);
+extern const void __xc32_LoadAllSecondarySections();
 
 #endif //__BIN2HEX_XC32_SECONDARY_RECORD_H
