45a02fd60ba3c839e2f93e2c98c049da49428dae
Luis Lacasa <luis.lacasa@microchip.com>
2024-11-28 00:33:18 +0100

    SMS-4079: Fill forced CSMA value in PRIME pserial

--- service/pserial/srv_pserial_prime.c
+++ service/pserial/srv_pserial_prime.c
@@ -167,7 +167,9 @@ void SRV_PSERIAL_ParseTxMessage(DRV_PLC_PHY_TRANSMISSION_OBJ* pDataDst, uint8_t*
     pDataDst->timeIni += ((uint32_t)*pDataSrc++) << 16;
     pDataDst->timeIni += ((uint32_t)*pDataSrc++) << 8;
     pDataDst->timeIni += (uint32_t)*pDataSrc++;
-    // pDataDst->forced += (uint32_t)*pDataSrc++; ToDo: Fix forced -> csma
+    pDataDst->csma.disableRx = (uint32_t)*pDataSrc++;
+    pDataDst->csma.senseCount = 0;
+    pDataDst->csma.senseDelayMs = 0;
     pDataDst->dataLength = ((uint16_t)*pDataSrc++) << 8;
     pDataDst->dataLength += (uint16_t)*pDataSrc++;
 
